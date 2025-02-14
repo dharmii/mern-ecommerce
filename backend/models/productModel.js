aa9b8d323408669a4c2bf43a2575ed5a2ff001f8
dharmii <dharmikachilaka@gmail.com>
2025-02-14 16:29:47 +0530

    commit

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,4 +1,4 @@
-import mongoose, { MongooseError } from 'mongoose';
+import mongoose from 'mongoose';
 const productSchema = new mongoose.Schema({
     name:{type: String, required: true},
     description: {type: String, required: true},
